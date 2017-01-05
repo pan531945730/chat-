3aef025c437968ad650ee4f4b699e39c76d0546d
qinglipan <pan531945730@126.com>
2017-01-05 09:52:03 +0800

    激光通讯

---
+++ js/answerChat.js
@@ -0,0 +1,236 @@
+;
+$(function() {
+    //极光文档地址https://docs.jiguang.cn/jmessage/client/im_sdk_js_v2/
+    var chat = function(op) {
+        var self = this;
+        var defaults = {
+            win: $('.chatBox'),
+            close: $('.close-btn'),
+            index: 0, //用户索引值
+            name: '', //用户姓名
+            nameHtml: $(".talkTo"),
+            userWin: $('.chat03-content'), //成员列表窗口
+            chatWin: $(".chat01-content"), //聊天窗口
+            textarea: $("#textarea"),
+            JIM: new JMessage(),
+            userArr: [], //成员数组
+            blinkNum: 0,
+            title: document.title
+        };
+        this.ops = $.extend(defaults, op);
+        self.init();
+    }
+    chat.prototype.init = function() {
+            var self = this;
+
+            Date.prototype.toHyphenDateString = function() {
+                var year = this.getFullYear();
+                var month = this.getMonth() + 1;
+                var date = this.getDate();
+                if (month < 10) {
+                    month = "0" + month;
+                }
+                if (date < 10) {
+                    date = "0" + date;
+                }
+                var hours = this.getHours();
+                var mins = this.getMinutes();
+                var second = this.getSeconds();
+                return year + "-" + month + "-" + date + " " + hours + ":" + mins + ":" + second;
+            };
+            self.ops.JIM.init({
+                "appkey": self.ops.appkey, //开发者在极光平台注册的IM应用appkey
+                "random_str": self.ops.randomStr, //20-36长度的随机字符串, 作为签名加salt使用
+                "signature": self.ops.signature, //当前时间戳
+                "timestamp": self.ops.timestamp //  签名
+            }).onSuccess(function(data) {
+
+                self.ops.JIM.login({
+                    'username': self.ops.username,
+                    'password': self.ops.password
+                }).onSuccess(function(data) {
+
+                    console.log('login suc');
+                }).onAck(function(data) {
+
+                    console.log(data);
+                }).onFail(function(data) {
+
+                    console.log('fail');
+                });
+
+            }).onFail(function(data) {
+                // do something
+                console.log('onfail');
+            });
+
+            //消息监听
+            self.ops.JIM.onMsgReceive(function(data) {
+                console.log(data);
+                self.blinkStop();
+                self.blinkBegin();
+                $.each(data.messages, function(i, v) {
+                    if (self.ops.userArr.indexOf(v.content.from_id) >= 0) {
+                        $('.message-box[data-id="' + v.content.from_id + '"]').append(self.userTep(v.content));
+
+                        if (!$('.person[data-id="' + v.content.from_id + '"]').hasClass('choosed')) {
+                            $('.person[data-id="' + v.content.from_id + '"]').find('label').addClass('news');
+                        }
+
+                    } else {
+                        self.ops.userWin.find('ul').append(self.addUserPeople(v.content));
+                        self.ops.chatWin.append(self.addUserWindow(v.content));
+                        $('.message-box[data-id="' + v.content.from_id + '"]').append(self.userTep(v.content));
+
+                        //当是第一名成员时
+                        if (self.ops.userArr.length == 0) {
+                            $('.person[data-id="' + v.content.from_id + '"]').addClass('choosed');
+                            $('.person[data-id="' + v.content.from_id + '"]').find('label').removeClass('news');
+                            $('.message-box[data-id="' + v.content.from_id + '"]').show();
+                            self.ops.index = v.content.from_id;
+                            self.ops.name = v.content.from_name;
+                            self.ops.nameHtml.text(self.ops.name);
+                        }
+
+                        self.ops.userArr.push(v.content.from_id);
+                    }
+                })
+                self.ops.chatWin.scrollTop($('.message-box[data-id="' + self.ops.index + '"]').height());
+
+            });
+
+            //成员列表hover交互
+            self.ops.userWin.on('mouseover', 'li', function() {
+                $(this).addClass("hover").siblings().removeClass("hover");
+            });
+            self.ops.userWin.on('mouseout', 'li', function() {
+                $(this).removeClass("hover").siblings().removeClass("hover")
+            });
+
+            //双击成员列表交互
+            self.ops.userWin.on('dblclick', 'li', function() {
+                self.ops.index = $(this).attr('data-id');
+                self.ops.name = $(this).find(".person-name").text();
+                self.ops.chatWin.scrollTop(0);
+                $(this).addClass("choosed").siblings().removeClass("choosed");
+                $(this).find('label').removeClass('news');
+                self.ops.nameHtml.text(self.ops.name);
+                $('.message-box[data-id="' + self.ops.index + '"]').show().siblings().hide();
+            })
+
+            //关闭窗口
+            self.ops.close.on('click', function() {
+                self.ops.win.hide();
+            })
+
+            //点击发送
+            $('.send-btn').click(function() {
+                self.sendFn();
+            })
+
+            //回车发送
+            document.onkeydown = function(a) {
+                var b = document.all ? window.event : a;
+                return 13 == b.keyCode ? (self.sendFn(), !1) : void 0
+            }
+
+        }
+        //新消息title闪烁
+    chat.prototype.blinkBegin = function() {
+            var self = this;
+            var a = 0;
+            if (-1 == document.title.indexOf("【")) {
+                self.ops.blinkNum = setInterval(function() {
+                    a++;
+                    3 == a && (a = 1);
+                    1 == a && (document.title = "【　　　】" + self.ops.title);
+                    2 == a && (document.title = "【新消息】" + self.ops.title);
+                }, 500);
+            }
+        }
+        //新消息title停止闪烁
+    chat.prototype.blinkStop = function() {
+            var self = this;
+            clearInterval(self.ops.blinkNum);
+            document.title = self.ops.title;
+        }
+        //用户问题内容模板
+    chat.prototype.userTep = function(v) {
+            var self = this;
+            var html = '',
+                fromTime = new Date(v.create_time).toHyphenDateString();
+            html += "<div class='message'>";
+            html += "<div class='user-time'><span>" + fromTime + "</span></div>";
+            html += "<div class='user-logo'><img src='img/head/2017.jpg'/></div>";
+            html += "<div class='wrap-text'>" + v.msg_body.text + "</div>";
+            html += "</div>";
+            return html;
+        }
+        //新增用户列表模板
+    chat.prototype.addUserPeople = function(v) {
+            var self = this;
+            var html = '';
+            html += '<li class="person" data-id="' + v.from_id + '">';
+            html += '<div class="person-box"><img src="img/head/2017.jpg"><label class="news"></label></div>';
+            html += '<p class="person-name">' + v.from_name + '</p></li>';
+            return html;
+        }
+        //新增用户聊天窗口
+    chat.prototype.addUserWindow = function(v) {
+            var self = this;
+            var html = '';
+            html += '<div class="message-box" data-id="' + v.from_id + '"></div';
+            return html;
+        }
+        //客服回复问题模板
+    chat.prototype.reply = function(v) {
+            var self = this;
+            var html = '',
+                replyTime = new Date(v.create_time).toHyphenDateString();
+            html += "<div class='message me'>";
+            html += "<div class='user-time'><span>" + replyTime + "</span></div>";
+            html += "<div class='user-logo'><img src='img/head/2018.jpg'/></div>";
+            html += "<div class='wrap-text'>" + v.msg_body.text + "</div>";
+            html += "</div>";
+            return html;
+        }
+        //发送方法
+    chat.prototype.sendFn = function() {
+        var self = this,
+            replymsg = self.ops.textarea.val();
+        if (null != replymsg && '' != replymsg) {
+            if (self.ops.name == '') {
+                alert('当前没有成员');
+                return;
+            }
+            self.ops.JIM.sendSingleMsg({
+                'target_username': self.ops.name,
+                'content': replymsg
+            }).onSuccess(function(data, msg) {
+                // do something
+                console.log(msg);
+                self.blinkStop();
+                $('.message-box[data-id="' + self.ops.index + '"]').append(self.reply(msg.content));
+                self.ops.chatWin.scrollTop($('.message-box[data-id="' + self.ops.index + '"]').height());
+                self.ops.textarea.val("");
+            }).onFail(function(data) {
+                // do something
+                console.log('fail');
+            });
+
+        } else {
+            alert("请输入聊天内容!");
+        }
+    }
+        
+    var hash = hex_md5("appkey=69cacec81309bde0b1ffa029&timestamp=1483512099743&random_str=Keo2PFnUJS6v2CBBb4thxdMB4380wV83&key=c88ed719d726a7c12d5dc258");
+    console.log(hash)
+    var chatInit = new chat({
+        appkey: '69cacec81309bde0b1ffa029',
+        randomStr: 'Keo2PFnUJS6v2CBBb4thxdMB4380wV83',
+        signature: hash,
+        timestamp: '1483512099743',
+        username: 'kf000013',
+        password: '123456'
+    });
+});
---
+++ js/chat.js
@@ -0,0 +1,243 @@
+;
+$(function(){
+    //初始化接口数据
+    var initDate = {
+        'member' : [
+            {
+                'favicon' : 'img/head/2013.jpg',
+                'name' : '刘秀',
+                'id' : 0,
+                'mesg' : '您好，请问怎么注册0000您好，请问怎么注册0000您好，请问怎么注册0000您好，请问怎么注册0000您好，请问怎么注册0000您好，请问怎么注册0000',
+                'isNew' : 0, //老用户
+                'isOnline' : 0, //不在线
+                'time' : '2016-12-20  16:57:0'
+            },
+            {
+                'favicon' : 'img/head/2014.jpg',
+                'name' : '陈诚',
+                'id' : 1,
+                'mesg' : '您好，请问怎么买11111',
+                'isNew' : 0, //老用户
+                'isOnline' : 0, //不在线
+                'time' : '2016-12-20  16:57:0'
+            },
+            {
+                'favicon' : 'img/head/2015.jpg',
+                'name' : '王旭',
+                'id' : 2,
+                'mesg' : '您好，请问怎么用2222',
+                'isNew' : 1, //新用户
+                'isOnline' : 1, //在线
+                'time' : '2016-12-20  16:57:0'
+            },
+            {
+                'favicon' : 'img/head/2016.jpg',
+                'name' : '张灵',
+                'id' : 3,
+                'mesg' : '您好，请问怎么买3333',
+                'isNew' : 1, //新用户
+                'isOnline' : 1, //在线
+                'time' : '2016-12-20  16:57:0'
+            },
+            {
+                'favicon' : 'img/head/2017.jpg',
+                'name' : 'xiaoming',
+                'id' : 3,
+                'mesg' : '您好，请问怎么买3333',
+                'isNew' : 1, //新用户
+                'isOnline' : 1, //在线
+                'time' : '2016-12-20  16:57:0'
+            }
+        ]
+    };
+    //客服回复问题数据
+    var replyData = {
+        'time' : '2016-12-20  16:57:0',
+        'favicon' : 'img/head/2019.jpg',
+        'mesg' : '稍等客服忙'
+    }
+    //新信息提示
+    var blinkTitle = {
+        show: function() {
+            var a = 0,
+                b = document.title,
+                c;
+            if (-1 == document.title.indexOf("【")){
+                c = setInterval(function() {
+                    a++;
+                    3 == a && (a = 1);
+                    1 == a && (document.title = "【　　　】" + b);
+                    2 == a && (document.title = "【新消息】" + b);
+                }, 500);
+            } 
+            return [c, b]
+        },
+        clear: function(a) {
+            a && (clearInterval(a[0]), document.title = a[1])
+        }
+    }
+    var chat = function(op){
+        var self = this;
+        var defaults = {
+            win : $('.chatBox'),
+            close : $('.close-btn'),
+            index : 0,//用户索引值
+            name : '', //用户姓名
+            nameHtml : $(".talkTo"),
+            userWin : $('.chat03-content'),//成员列表窗口
+            chatWin : $(".chat01-content"),//聊天窗口
+            textarea : $("#textarea")
+        };
+        this.ops = $.extend(defaults,op);
+        self.init();
+    }
+    chat.prototype.init = function(){
+        var self = this;
+        self.ajaxChat();
+        /*setInterval(function(){
+            self.ajaxChat();
+        },1000)*/
+
+        //成员列表hover交互
+        self.ops.userWin.find('li').on('mouseover',function(){
+            $(this).addClass("hover").siblings().removeClass("hover");
+        });
+        self.ops.userWin.find('li').on('mouseout',function(){
+            $(this).removeClass("hover").siblings().removeClass("hover")
+        });
+
+        //双击成员列表交互
+        self.ops.userWin.find('li').on('dblclick',function(){
+            self.ops.index = $(this).attr('data-id');
+            self.ops.name = $(this).find(".person-name").text();
+            self.ops.chatWin.scrollTop(0);
+            $(this).addClass("choosed").siblings().removeClass("choosed");
+            self.ops.nameHtml.text(self.ops.name);
+            $('.message-box[data-id="'+self.ops.index+'"]').show().siblings().hide();
+        })
+
+        //关闭窗口
+        self.ops.close.on('click',function(){
+            self.ops.win.hide();
+        })
+
+        //点击发送
+        $('.send-btn').click(function() {
+            self.sendFn();        
+        })
+
+        //回车发送
+        document.onkeydown = function(a) {
+            var b = document.all ? window.event : a;
+            return 13 == b.keyCode ? (self.sendFn(), !1) : void 0
+        }
+
+    }
+    //用户问题内容模板
+    chat.prototype.userTep = function(v){
+        var self = this;
+        var html = '';
+        html += "<div class='message'>";
+        html += "<div class='user-time'><span>" + v.time + "</span></div>";
+        html += "<div class='user-logo'><img src='" + v.favicon + "'/></div>";
+        html += "<div class='wrap-text'>" + v.mesg + "</div>";
+        html += "</div>";
+        return html;
+    }
+    //新增用户列表模板
+    chat.prototype.addUserPeople = function(v){
+        var self = this;
+        var html = '',
+            isOnline = v.isOnline ? 'online':'';
+        html += '<li class="person" data-id="'+v.id+'">';
+        html += '<div class="person-box"><img src="'+v.favicon+'"><label class="'+isOnline +'"></label></div>';
+        html += '<p class="person-name">'+v.name+'</p></li>';
+        return html; 
+    }
+    //新增用户聊天窗口
+    chat.prototype.addUserWindow = function(v){
+        var self = this;
+        var html = '';
+        html += '<div class="message-box" data-id="'+v.id+'"></div';
+        return html;
+    }
+    //客服回复问题模板
+    chat.prototype.reply = function(v){
+        var self = this;
+        var html = '';
+        html += "<div class='message me'>";
+        html += "<div class='user-time'><span>" + v.time + "</span></div>";
+        html += "<div class='user-logo'><img src='" + v.favicon + "'/></div>";
+        html += "<div class='wrap-text'>" + v.mesg + "</div>";
+        html += "</div>";
+        return html;
+    }
+    chat.prototype.ajaxChat = function(){
+        var self = this;
+        $.ajax({
+            type : 'post',
+            dataType : 'json',
+            url : '#',
+            data: null,
+            beforeSend : function(){
+                console.log('beforeSend');
+            },
+            success : function(v){
+                console.log('success');
+            },
+            error : function(){
+                self.messageTip();
+                $.each(initDate.member,function(i,v){
+                    if(v.isNew == 0){
+                        $('.message-box[data-id="'+v.id+'"]').append(self.userTep(v));
+                    }else{
+                        self.ops.userWin.find('ul').append(self.addUserPeople(v));
+                        self.ops.chatWin.append(self.addUserWindow(v));
+                        $('.message-box[data-id="'+v.id+'"]').append(self.userTep(v));
+                    }
+                })
+                console.log('error');
+            }
+        })
+    },
+    chat.prototype.ajaxReply = function(){
+        var self = this;
+        $.ajax({
+            type : 'post',
+            dataType : 'json',
+            url : '#',
+            data: null,
+            beforeSend : function(){
+                console.log('beforeSend');
+            },
+            success : function(v){
+                console.log('success');
+            },
+            error : function(){                
+                console.log('error');
+                $('.message-box[data-id="'+self.ops.index+'"]').append(self.reply(replyData));
+                self.ops.chatWin.scrollTop($('.message-box[data-id="'+self.ops.index+'"]').height());
+                self.ops.textarea.val("");
+            }
+        })
+    }
+    chat.prototype.messageTip = function(){
+        var self = this;
+        var a = blinkTitle.show();
+        setTimeout(function() {
+            blinkTitle.clear(a)
+        }, 8000)
+    }
+    //发送方法
+    chat.prototype.sendFn = function(){
+        var self = this;
+        replyData.mesg = self.ops.textarea.val();
+        if( null != replyData.mesg && ''!= replyData.mesg){
+            self.ajaxReply();
+        }else{
+            alert("请输入聊天内容!");
+        }
+    }
+
+    var chatInit = new chat();
+});
